d6359e0ab9f72ed332ecb157130587350292069b
nakdevluis <78453562+nakdevluis@users.noreply.github.com>
2022-08-21 17:01:37 -0400

    Add files via upload

---
+++ main(1).py
@@ -0,0 +1,8 @@
+
+print("Welcome to the tip calculator!")
+bill = float(input("what was the total bill? $"))
+tip = int(input("How much tip would you like to give? 10,12, or 15? "))
+dutch = int(input("how many people to split the bill? "))
+bill_with_tip = tip/100 * bill + bill
+math = int(bill)/int(split) * 1 + int(tip)
+print(round(math)
